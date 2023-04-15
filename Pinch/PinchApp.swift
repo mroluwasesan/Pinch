db61e5783bf9a51ee69b04cd66081f270ed7f3a0
Rotimi <mr.oluwasesan@gmail.com>
2023-04-15 01:29:10 +0100

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Negatron on 15/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

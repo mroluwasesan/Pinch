ff8f8deac0e486cd24ea79c29111c3671bc38483
Rotimi <mr.oluwasesan@gmail.com>
2023-04-15 20:26:10 +0100

    Adding the image to the body and implementing the first gesture which is a Double Tap Gesture

--- Pinch/Screen/ContentView.swift
+++ Pinch/Screen/ContentView.swift
@@ -8,17 +8,51 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     // MARK: - PROPERTY
+    @State private var isAnimating: Bool = false
+    @State private var ImageScale : CGFloat = 1
+    
     // MARK: - FUNCTION
+    
     // MARK: - CONTENT
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        NavigationView{
+            ZStack{
+                //MARK: - PAGE IMAGE
+                Image("magazine-front-cover")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(10)
+                    .padding()
+                    .shadow(color: .black.opacity(0.2), radius: 12, x: 2,y: 2)
+                    .opacity(isAnimating ? 1 : 0)
+                    .animation(.linear(duration: 1), value: isAnimating)
+                    .scaleEffect(ImageScale)
+                // MARK: - 1 TAP GESTURE
+                    .onTapGesture(count: 2, perform: {
+                        if ImageScale == 1{
+                            withAnimation(.spring()){
+                                ImageScale = 5
+                            }
+                        }
+                        else{
+                            withAnimation(.spring()){
+                                ImageScale = 1
+                            }
+                        }
+                    })//: TAP Gesture
+            }//:ZSTAK
+            .navigationTitle("Pinch & Zoom")
+            .navigationBarTitleDisplayMode(.inline)
+            .onAppear(perform: {
+                withAnimation(.linear(duration: 1)){
+                    isAnimating = true
+                }
+            })
+            
+        }//: NAVIGATION
+        .navigationViewStyle(.stack)
     }
 }
 
@@ -30,5 +64,6 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
     }
 }
